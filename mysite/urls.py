cdc3958ad77e3eeb4d86f39c7b81ab35b859eb96
Ethan Choo <ec4kj@virginia.edu>
2020-09-21 17:10:47 -0400

    added urls

--- mysite/urls.py
+++ mysite/urls.py
@@ -14,8 +14,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
+    path('', lambda r: HttpResponseRedirect('5-3-1/login/')),
+    path('5-3-1/' include('workout.urls'))
     path('admin/', admin.site.urls),
 ]
